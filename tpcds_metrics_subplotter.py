78daf714089bb6727bd5b0193012a73e6c823cb9
binbjz <8213913+binbjz@users.noreply.github.com>
2023-11-10 12:14:33 +0800

    Update tpcds_metrics_subplotter.py
    Optimize memory usage and improve operational efficiency.

--- tpcds_metrics_subplotter.py
+++ tpcds_metrics_subplotter.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from typing import Optional
@@ -6,54 +5,43 @@ from pathlib import Path
 from loguru import logger
 
 
-def plot_metrics_from_csv_chunked(csv_path: str, ncols: int, chunk_size: int = 5000,
-                                  output_dir: Optional[Path] = None,
-                                  output_image_name: Optional[str] = None):
+def plot_metrics_from_csv_chunked_continuous(csv_path: str, ncols: int, chunk_size: int = 5000,
+                                             output_dir: Optional[Path] = None,
+                                             output_image_name: Optional[str] = None):
     """
-    从给定的 CSV 文件路径中读取数据,并实时绘制数值型指标的子图。
-    逐块读取数据以优化内存使用。
+    从给定的 CSV 文件路径中分块读取数据,并且绘制数据指标的连续图表。
+    使用全局索引偏移量来在 x 轴上实现连续性。
     """
-    fig, axes = None, None
-    cumulative_df = pd.DataFrame()
-
     rows_read, total_rows = 0, sum(1 for _ in open(csv_path)) - 1
 
     for chunk in pd.read_csv(csv_path, chunksize=chunk_size):
-        if not any(chunk.dtypes.astype(str).isin(["float64", "int64"])):
-            logger.info("没有数值型列可供绘图..")
-            return
-
-        logger.info(f"列名：{chunk.columns.tolist()}")
-        logger.info(f"数据块最后一行数据：\n{chunk.iloc[-1].to_dict()}")
-
-        rows_read += len(chunk)
-        logger.info(f"total_rows：{total_rows}")
-        logger.info(f"rows_read：{rows_read}")
-
-        if rows_read == total_rows:
+        if rows_read + len(chunk) >= total_rows:
+            last_row = chunk.iloc[-1]
             chunk = chunk.iloc[:-1]
+            logger.info("最后一行数据：\n{}".format(last_row))
 
         numeric_cols_chunk = chunk.select_dtypes(include=["number"]).dropna(axis=1)
-        cumulative_df = pd.concat([cumulative_df, numeric_cols_chunk])
+        if len(numeric_cols_chunk.columns) == 0:
+            logger.info("没有数值型列可供绘图。")
+            continue
 
-        if fig is None and axes is None:
+        axes = None
+        if rows_read == 0:
             num_columns = len(numeric_cols_chunk.columns)
             nrows = num_columns // ncols + (num_columns % ncols > 0)
             fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 6 * nrows))
 
-        for ax, col in zip(axes.flatten(), numeric_cols_chunk.columns):
-            ax.clear()
-            ax.plot(cumulative_df[col], alpha=0.5)
-            ax.set_xticks(np.arange(0, len(cumulative_df[col]), 0.5))
+        for i, col in enumerate(numeric_cols_chunk.columns):
+            ax = axes.flatten()[i]
+            x_ticks = range(rows_read, rows_read + len(chunk))
+            ax.plot(x_ticks, numeric_cols_chunk[col], alpha=0.5)
             ax.set_title(col, fontsize=12)
-            ax.set_xlabel("Time (Minutes)", fontsize=9)
-            ax.set_ylabel("Metric Value", fontsize=9)
 
-    plt.subplots_adjust(wspace=0.3, hspace=0.5)
+        rows_read += len(chunk)
+
     plt.tight_layout()
-
     save_path = (output_dir if output_dir else Path(".")).joinpath(
-        output_image_name if output_image_name else "tpcds_metrics_data.png")
+        output_image_name if output_image_name else "metrics_data_continuous.png")
     plt.savefig(save_path, dpi=300)
     logger.info(f"指标分析图已保存至 {save_path}")
     plt.show()
@@ -61,5 +49,4 @@ def plot_metrics_from_csv_chunked(csv_path: str, ncols: int, chunk_size: int = 5
 
 if __name__ == "__main__":
     _csv_file = "tpcds_metrics_data.csv"
-    plot_metrics_from_csv_chunked(_csv_file, ncols=2)
-
+    plot_metrics_from_csv_chunked_continuous(_csv_file, ncols=2)
